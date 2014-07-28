6e6942b75d5a281d03ef41be6d3c6868b780ba94
Kevin Sołtysiak <kevin@ksol.fr>
2014-07-28 14:54:57 +0200

    Fix MessageDelivery for ActionMailer
    * API endpoint was wrong;
    * No properties were whitelisted.
    
    There is probably more properties to whitelist;
    I only whitelisted the common ones.

--- lib/mailjet/message_delivery.rb
+++ lib/mailjet/message_delivery.rb
@@ -3,7 +3,8 @@ require 'mailjet/resource'
 module Mailjet
   class MessageDelivery
     include Mailjet::Resource
-    self.resource_path = 'v3/send'
+    self.resource_path = 'v3/send/message'
     self.public_operations = [:post]
+    self.properties = [:from, :sender, :to, :cc, :bcc, :subject, :text]
   end
 end
