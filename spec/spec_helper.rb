246f944cbaa5e65552e50ba6c1d356d817afba16
François de Metz <francois@2metz.fr>
2014-07-30 15:13:39 +0200

    Use YAML::load_file.
    Signed-off-by: François de Metz <francois@stormz.me>
    Signed-off-by: Julien Boyer <julien@stormz.me>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ require 'turn/autorun'
 
 require File.expand_path './support/vcr_setup.rb', __dir__
 
-test_account = YAML::load(File.new(File.expand_path("../../config.yml", __FILE__)))['mailjet']
+test_account = YAML::load_file(File.expand_path("../../config.yml", __FILE__))['mailjet']
 
 MiniTest::Spec.before do
   Mailjet.configure do |config|
