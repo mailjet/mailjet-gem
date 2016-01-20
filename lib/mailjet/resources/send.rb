d920282aa65e638da3aa59cff1cdf2f5dd52a555
Arnaud BRETON <arn.breton@gmail.com>
2016-01-20 23:22:54 +0100

    Missing 'headers' resource property

--- lib/mailjet/resources/send.rb
+++ lib/mailjet/resources/send.rb
@@ -5,6 +5,6 @@ module Mailjet
     include Mailjet::Resource
     self.resource_path = 'v3/send/'
     self.public_operations = [:post]
-    self.resourceprop = [:from_email, :from_name, :recipients, :subject, :text_part, :html_part, :attachments, :inline_attachments, 'mj-template-id', 'mj-custom-id']
+    self.resourceprop = [:from_email, :from_name, :recipients, :subject, :text_part, :html_part, :attachments, :inline_attachments, :headers, 'mj-template-id', 'mj-custom-id']
   end
 end
