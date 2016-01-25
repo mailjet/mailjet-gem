68e12cba3291f3e2e4ff3bd995b66a20330fc378
Guillaume Badi <gbadi@mailjet.com>
2016-01-25 17:44:44 +0100

    `to` prop

--- lib/mailjet/resources/send.rb
+++ lib/mailjet/resources/send.rb
@@ -5,6 +5,6 @@ module Mailjet
     include Mailjet::Resource
     self.resource_path = 'v3/send/'
     self.public_operations = [:post]
-    self.resourceprop = [:from_email, :from_name, :recipients, :subject, :text_part, :html_part, :attachments, :inline_attachments, :headers, 'Mj-TemplateID', 'Mj-CustomID', :message_id, :messages]
+    self.resourceprop = [:from_email, :from_name, :recipients, :subject, :text_part, :html_part, :attachments, :inline_attachments, :headers, 'Mj-TemplateID', 'Mj-CustomID', :message_id, :messages, :to, :cc, :bcc]
   end
 end
