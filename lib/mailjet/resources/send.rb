bfb899f5f99db560a2f6a9e3e850f984fb08ac8a
Guillaume Badi <gbadi@mailjet.com>
2016-03-01 14:35:20 +0100

    Update send.rb

--- lib/mailjet/resources/send.rb
+++ lib/mailjet/resources/send.rb
@@ -5,6 +5,6 @@ module Mailjet
     include Mailjet::Resource
     self.resource_path = 'v3/send/'
     self.public_operations = [:post]
-    self.resourceprop = [:from_email, :from_name, :recipients, :subject, :vars, :text_part, :html_part, :attachments, :inline_attachments, :headers, 'Mj-TemplateID', 'Mj-CustomID', :message_id, :messages, :to, :cc, :bcc, 'Mj-TemplateLanguage', 'MJ-TemplateErrorReporting', 'MJ-TemplateErrorDeliver']
+    self.resourceprop = [:from_email, :from_name, :recipients, :subject, :vars, :text_part, :html_part, :attachments, :inline_attachments, :headers, 'Mj-TemplateID', 'Mj-CustomID', :message_id, :messages, :to, :cc, :bcc, 'Mj-TemplateLanguage', 'MJ-TemplateErrorReporting', 'MJ-TemplateErrorDeliver', 'Mj-campaign', 'Mj-deduplicatecampaign', 'Mj-trackopen', 'Mj-trackclick', 'Mj-EventPayLoad']
   end
 end
