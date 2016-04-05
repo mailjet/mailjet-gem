a198da052d389c2de10a35155dee798eb88d2d4e
Guillaume Badi <gbadi@student.42.fr>
2016-04-05 15:19:59 +0200

    spec

--- spec/mailjet-spec.rb
+++ spec/mailjet-spec.rb
@@ -3,6 +3,7 @@ require 'mailjet_spec_helper'
 
 API_KEY = ENV['MJ_APIKEY_PUBLIC']
 API_SECRET = ENV['MJ_APIKEY_PRIVATE']
+random_email = (0...8).map { (65 + rand(26)).chr }.join + '@test.com'
 
 describe Mailjet do
   before(:all) do
@@ -50,6 +51,62 @@ describe Mailjet do
       expect(res).not_to be_nil
     end
 
+  end
+
+  describe 'Simple SEND request' do
+    it 'sends a basic email' do
+      res = Mailjet::Send.create(
+        :from_email => 'gbadi@mailjet.com',
+        :from_name => 'gbadimailjetcom',
+        :subject => 'Travis Ruby',
+        :recipients => [ { 'Email' => 'gbadi@mailjet.com' } ],
+        :text_part => 'Message' )
+      expect(res).not_to be_nil
+    end
+
+    it 'sends a basic email to 2 recipients' do
+      res = Mailjet::Send.create(
+        :from_email => 'gbadi@mailjet.com',
+        :from_name => 'gbadimailjetcom',
+        :subject => 'Travis Ruby',
+        :recipients => [ { 'Email' => 'gbadi@mailjet.com' }, { 'Email' => 'gbadi@mailjet.com' } ],
+        :text_part => 'Message two 2 people' )
+      expect(res).not_to be_nil
+    end
+
+    it 'sends a basic email to 2 recipients as messages' do
+      res = Mailjet::Send.create(
+        :messages => [{
+          'FromEmail' => 'gbadi@mailjet.com',
+          'FromName' => 'gbadimailjetcom',
+          'Subject' => 'Travis Ruby',
+          'Recipients' => [ { 'Email' => 'gbadi@mailjet.com' } ],
+          'Text-Part' => 'Messages prop'
+        }, {
+          'FromEmail' => 'gbadi@mailjet.com',
+          'FromName' => 'gbadimailjetcom',
+          'Subject' => 'Travis Ruby',
+          'Recipients' => [ { 'Email' => 'gbadi@mailjet.com' } ],
+          'Text-Part' => 'Messages prop'
+        }])
+      expect(res).not_to be_nil
+    end
+  end
+
+  describe 'Simple POST on Contact' do
+    it 'Creates a random Contact' do
+      res = Mailjet::Contact.create(email: random_email, name: 'Test email')
+      expect(res).not_to be_nil
+    end
+  end
+
+  describe 'PUT contact' do
+    it 'Should update the contact name' do
+      res = Mailjet::Contact.find(random_email)
+      res.name = 'Updated name'
+      response = res.save!
+      expect(response).not_to be_nil
+    end
   end
 
 end
