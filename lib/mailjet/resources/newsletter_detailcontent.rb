0bb203e76f52a66364b699e1eaa809884b8008b5
Guillaume Badi <gbadi@student.42.fr>
2016-05-26 10:42:19 +0200

    temp late_detailcontent + template

--- lib/mailjet/resources/newsletter_detailcontent.rb
+++ lib/mailjet/resources/newsletter_detailcontent.rb
@@ -4,10 +4,10 @@ module Mailjet
   class Newsletter_detailcontent
     include Mailjet::Resource
     self.action = "detailcontent"
-    self.resource_path = "v3/REST/newsletter/id/#{self.action}"
-    self.public_operations = [:get, :put, :post, :delete]
+    self.resource_path = "v3/REST/template/id/#{self.action}"
+    self.public_operations = [:get, :post]
     self.filters = []
-    self.resourceprop = [:text_part, :html_part]
+    self.resourceprop = [:text_part, :html_part, :headers, :mjml_content]
 
   end
 end
---
+++ lib/mailjet/resources/template.rb
@@ -0,0 +1,12 @@
+require 'mailjet/resource'
+
+module Mailjet
+  class Template
+    include Mailjet::Resource
+    self.resource_path = 'v3/REST/template'
+    self.public_operations = [:get, :put, :post, :delete]
+    self.filters = [:api_key, :categories, :categories_selection_method, :edit_mode, :name, :owner_type, :purposes, :purposes_selection_method, :user]
+    self.resourceprop = [:author, :categories, :copyright, :description, :edit_mode, :is_starred, :name, :owner_type, :presets, :purposes]
+
+  end
+end
