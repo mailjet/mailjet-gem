c7e0eba18aa75a92834eac5a7fa29258cbddbdcf
Guillaume Badi <gbadi@student.42.fr>
2016-07-28 16:54:58 +0200

    action mailer default from

--- lib/mailjet/mailer.rb
+++ lib/mailjet/mailer.rb
@@ -21,11 +21,11 @@ ActionMailer::Base.add_delivery_method :mailjet, Mailjet::Mailer
 
 class Mailjet::APIMailer
   def initialize(options = {})
-    ActionMailer::Base.default(:from => Mailjet.config.default_from) if Mailjet.config.default_from.present?
     @delivery_method_options = options.slice(:'mj-prio', :'mj-campaign', :'mj-deduplicatecampaign', :'mj-trackopen', :'mj-trackclick', :'mj-customid', :'mj-eventpayload', :'header')
   end
 
   def deliver!(mail)
+
     if mail.multipart?
       content = {
         :text => mail.text_part.try(:decoded),
@@ -34,23 +34,31 @@ class Mailjet::APIMailer
         :inlineattachment => mail.attachments.select{ |a| !a.inline? }.try(:decoded)
       }
     else
-      content = (mail.mime_type == "text/html") ? {:html => mail.body.decoded} : {:text => mail.body.decoded}
+      content = (mail.mime_type == "text/html") ? {:html_part => mail.body.raw_source} : {:text_part => mail.body.raw_source}
     end
 
-    payload = {
-      :from => mail.from || Mailjet.config.default_from,
-      :sender => mail.sender,
-      :to => mail.to,
-      :reply_to => mail.reply_to,
-      :cc => mail.cc,
-      :bcc => mail.bcc,
-      :subject => mail.subject,
-      :'mj-customid' => mail['X-MJ-CustomID'] && mail['X-MJ-CustomID'].value,
-      :'mj-eventpayload' => mail['X-MJ-EventPayload'] && mail['X-MJ-EventPayload'].value
-    }.merge(content).merge(@delivery_method_options)
-
-    Mailjet::MessageDelivery.create(payload)
+  if Mailjet.config.default_from.present?
+    from_address = Mail::AddressList.new(Mailjet.config.default_from).addresses[0]
+  else
+    from_address = Mail::AddressList.new(mail.from.join(', ')).addresses[0]
   end
+
+  base_from = { :from_name => from_address.display_name,
+                :from_email => from_address.address }
+
+	payload = {
+    :to => mail.to.join(', '),
+    :reply_to => mail.reply_to,
+    :cc => mail.cc,
+    :bcc => mail.bcc,
+    :subject => mail.subject,
+    :'mj-customid' => mail['X-MJ-CustomID'] && mail['X-MJ-CustomID'].value,
+    :'mj-eventpayload' => mail['X-MJ-EventPayload'] && mail['X-MJ-EventPayload'].value
+	}.merge(content).merge(base_from).merge(@delivery_method_options)
+
+	Mailjet::Send.create(payload)
+
+	end
 end
 
 ActionMailer::Base.add_delivery_method :mailjet_api, Mailjet::APIMailer
