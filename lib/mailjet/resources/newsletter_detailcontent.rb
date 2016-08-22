2b73dc2796836dfb0f8bc9874a37426dbef3efce
Arnaud Breton <abreton@mailjet.com>
2016-08-22 13:44:53 +0200

    Fix path for newsletter detailcontent action

--- lib/mailjet/resources/newsletter_detailcontent.rb
+++ lib/mailjet/resources/newsletter_detailcontent.rb
@@ -4,7 +4,7 @@ module Mailjet
   class Newsletter_detailcontent
     include Mailjet::Resource
     self.action = "detailcontent"
-    self.resource_path = "v3/REST/template/id/#{self.action}"
+    self.resource_path = "v3/REST/newsletter/id/#{self.action}"
     self.public_operations = [:get, :post]
     self.filters = []
     self.resourceprop = [:text_part, :html_part, :headers, :mjml_content]
