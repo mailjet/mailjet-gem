41cd319cdf9c25ef41e732a84d699bc433751539
Aridjar <cpaumard@mailjet.com>
2017-01-10 18:13:56 +0100

    remove char leading to 404

--- lib/mailjet/resources/send.rb
+++ lib/mailjet/resources/send.rb
@@ -4,7 +4,7 @@ module Mailjet
   class Send
     include Mailjet::Resource
 #    self.version = 'v3.1'
-    self.resource_path = 'send/'
+    self.resource_path = 'send'
     self.public_operations = [:post]
   end
 end
