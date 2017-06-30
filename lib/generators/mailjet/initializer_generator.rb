5a9806d91101f73e3af1500c4a5de436ae86f6c7
Lorel <lorel@wellatribe.com>
2017-06-30 18:51:41 +0200

    text improvement for generator and config template

--- lib/generators/mailjet/initializer_generator.rb
+++ lib/generators/mailjet/initializer_generator.rb
@@ -5,16 +5,31 @@ module Mailjet
     source_root File.expand_path('../templates', __FILE__)
 
     def generate_initializer_file
+      config_file_path = 'config/initializers/mailjet.rb'
+
       say('Hey! We’re about to configure your Mailjet credentials for your application.')
-      say('You can find them on your account (https://app.mailjet.com/transactional).')
+      say('You can find them on your account (https://app.mailjet.com/account/api_keys).')
       say('Please help yourself by providing some intel:')
 
       @api_key =      ask('API key: ')
       @secret_key =   ask('Secret key: ')
       @default_from = ask('Sender address:')
-      @api_v3_1 =     yes?('Do you want to use Mailjet API v3.1? (y/n)')
 
-      template 'mailjet.rb.erb', 'config/initializers/mailjet.rb'
+      say("Don't forget that your sender address '#{@default_from}' has to be validated first on https://app.mailjet.com/account/sender.")
+
+      if @api_v3_1 = yes?('Do you want to use Mailjet API v3.1 for sending your emails? (y/n)')
+        @api_v3_1_notice = %{
+Mailjet API v3.1 is at the moment limited to Send API.
+We’ve not set the version to it directly since there is no other endpoint in that version.
+We recommend you create a dedicated instance of the wrapper set with it to send your emails.
+If you're only using the gem to send emails, then you can safely set it to this version.
+Otherwise, you can remove the dedicated line into #{config_file_path}.
+
+}
+        say(@api_v3_1_notice)
+      end
+
+      template 'mailjet.rb.erb', config_file_path
     end
   end
 end
