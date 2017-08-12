6b9d095bd89bce20ca0603eaeeee76abad5d5016
Kenny Meyer <kenny@kennymeyer.net>
2017-08-12 08:22:53 -0400

    Fix resource paths for CampaignDraft modules

--- lib/mailjet/resources/campaigndraft_schedule.rb
+++ lib/mailjet/resources/campaigndraft_schedule.rb
@@ -2,7 +2,7 @@ module Mailjet
   class Campaigndraft_schedule
     include Mailjet::Resource
     self.action = "schedule"
-    self.resource_path = "v3/REST/campaigndraft/id/#{self.action}"
+    self.resource_path = "REST/campaigndraft/id/#{self.action}"
     self.public_operations = [:post, :delete , :get]
     self.filters = []
     self.resourceprop = [:date]
