8bf81b9750e84c990fa4171b178f48af3a1f1f90
Michal Kozakiewicz <morswin@gmail.com>
2017-12-15 14:57:10 +0100

    Correctly handler errors

--- lib/mailjet/api_error.rb
+++ lib/mailjet/api_error.rb
@@ -10,19 +10,27 @@ module Mailjet
     def initialize(code, res, request, request_path, params)
       self.code = code
       self.reason = ""
-      unless res.blank?
-        resdec = ActiveSupport::JSON.decode(res)
-        self.reason = resdec['ErrorMessage']
+      message = "error #{code} while sending #{request.inspect} to #{request_path} with #{params.inspect}\n\n"
+      if res
+        begin
+          resdec = ActiveSupport::JSON.decode(res)
+          self.reason = resdec['ErrorMessage']
+          message += "ErorMessage: #{self.reason}\n"
+          if resdec['errors'].present?
+            message += "errors: " + [(resdec['errors'] || [])].flatten.map do |param, text|
+              [param, text].map(&:to_s).reject(&:blank?).join(': ')
+            end.join("\n")
+          else
+            message += res.inspect
+          end
+        rescue ActiveSupport::JSON.parse_error
+          message += res.inspect
+        end
       end
-      # code is ugly, output is pretty
-      super("error #{code} while sending #{request.inspect} to #{request_path} with #{params.inspect}\n\n" +
-        if res['errors'].present?
-          [(res['errors'] || [])].flatten.map do |param, text|
-            [param, text].map(&:to_s).reject(&:blank?).join(': ')
-          end.join("\n")
-        else
-          res.inspect
-        end + "\n\nPlease see https://dev.mailjet.com/guides/#status-codes for more informations on error numbers.\n\n"
+
+      super(
+        message + 
+          "\n\nPlease see https://dev.mailjet.com/guides/#status-codes for more informations on error numbers.\n\n"
       )
     end
   end
