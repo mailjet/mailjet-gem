6d578c99087e0f6e113b3eefb4fc402df485c791
Yevhen Nakonechnyi <eugene.src@gmail.com>
2020-10-16 10:23:31 +0300

    DE-32 Add contacts tests

--- spec/mailjet_spec_helper.rb
+++ spec/mailjet_spec_helper.rb
@@ -1,10 +1,26 @@
-require 'bundler/setup'
-require 'mailjet'
-require 'active_support/core_ext'
-require 'dotenv'
+require "bundler/setup"
+require "mailjet"
+require "active_support/core_ext"
+require "dotenv"
+require "vcr"
 Bundler.setup
 
 Dotenv.load
 
-RSpec.configure do |config|
+Mailjet.configure do |config|
+  config.api_key = ENV["MJ_APIKEY_PUBLIC"]
+  config.secret_key = ENV["MJ_APIKEY_PRIVATE"]
+  config.api_version = "v3"
+  config.default_from = "pilot@example.com"
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/cassettes"
+  c.hook_into :webmock
+
+  c.filter_sensitive_data("<BASIC>") do |interaction|
+    interaction.request.headers["Authorization"].first
+  end
+
+  c.configure_rspec_metadata!
 end
