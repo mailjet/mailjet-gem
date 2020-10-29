b0e7fd0735d2e8864c35b9134628145eabc30c0a
Yevhen Nakonechnyi <eugene.src@gmail.com>
2020-10-30 01:48:34 +0200

    DE-32 Update VCR match_requests_on

--- spec/mailjet_spec_helper.rb
+++ spec/mailjet_spec_helper.rb
@@ -18,6 +18,10 @@ VCR.configure do |c|
   c.cassette_library_dir = "spec/cassettes"
   c.hook_into :webmock
 
+  c.default_cassette_options = {
+    :match_requests_on => [:uri, :method, :body],
+  }
+
   c.filter_sensitive_data("<BASIC>") do |interaction|
     interaction.request.headers["Authorization"].first
   end
