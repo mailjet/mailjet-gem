7246111492ea4af64ece90250753d65ffa332a42
Simon Fish <si@mon.fish>
2021-08-18 12:37:52 +0100

    Set default version from mailer instance if present

--- lib/mailjet/mailer.rb
+++ lib/mailjet/mailer.rb
@@ -65,7 +65,7 @@ class Mailjet::APIMailer
     @delivery_method_options_v3_1 = options.slice(*V3_1_PERMITTED_OPTIONS)
   end
 
-  def settings(version: Mailjet.config.api_version)
+  def settings(version: @version || Mailjet.config.api_version)
     version == 'v3.1' ? @delivery_method_options_v3_1 : @delivery_method_options_v3_0
   end
 
