982be975ff76f748c876a61edfd6d971246f78f0
Mikhail Grishko <mikhail.grishko@gmail.com>
2024-05-01 16:08:10 +0300

    Fixed issue with sending unmodified parameters

--- lib/mailjet/resources/listrecipient.rb
+++ lib/mailjet/resources/listrecipient.rb
@@ -4,7 +4,8 @@ module Mailjet
     self.resource_path = 'REST/listrecipient'
     self.public_operations = [:get, :put, :post, :delete]
     self.filters = [:active, :blocked, :contact, :contact_email, :contacts_list, :last_activity_at, :list_name, :opened, :status, :unsub]
-    self.resourceprop = [:contact, :id, :is_active, :is_unsubscribed, :list, :unsubscribed_at, :contact_id, :list_id, 'ContactID', 'ListID', 'ContactALT', 'ListALT']
+    self.resourceprop = [:contact, :id, :is_active, :is_unsubscribed, :list, :contact_id, :list_id, 'ContactID', 'ListID', 'ContactALT', 'ListALT']
 
+    self.read_only_attributes = [:subscribed_at, :unsubscribed_at]
   end
 end
